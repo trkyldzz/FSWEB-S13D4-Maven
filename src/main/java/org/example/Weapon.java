c568db1d4df8d4360efa60a3ac33d7d6cf632ea0
Tarik <tariky92@gmail.com>
2026-06-11 19:57:16 +0300

    Complete encapsulation project

---
+++ src/main/java/org/example/Weapon.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public enum Weapon {
+    SWORD(25, 1.5),
+    AXE(35, 1.0),
+    BOW(15, 2.0);
+
+    private final int damage;
+    private final double attackSpeed;
+
+    Weapon(int damage, double attackSpeed) {
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public double getAttackSpeed() {
+        return attackSpeed;
+    }
+}
